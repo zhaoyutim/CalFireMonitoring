9901d87d6baa4e089a78772e5a6770c5027705c5
赵宇 <zhaoyu@zhaoyudeMBP.lan>
2020-09-04 23:06:18 +0200

    Add doc and configuration file

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -1,34 +1,30 @@
 import ee
-from pprint import pprint
-import tensorflow as tf
 from eeMapClient import eeMapClient
+import yaml
 
 ee.Initialize()
 
+# Load configuration file
+with open("configuration.yml", "r", encoding="utf8") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+rectangular_size = config.get('rectangular_size')
+max_cloud_probability = config.get('max_cloud_probability')
+latitude = config.get('LNU_lighting_complex').get('latitude')
+longitude = config.get('LNU_lighting_complex').get('longitude')
+start_time = config.get('LNU_lighting_complex').get('start')
+end_time = config.get('LNU_lighting_complex').get('end')
+point = ee.Geometry.Point(latitude, longitude)
+
+# Raw Dataset import
 sentinel2c = ee.ImageCollection("COPERNICUS/S2")
 sentinel2a = ee.ImageCollection("COPERNICUS/S2_SR")
 s2_clouds = ee.ImageCollection('COPERNICUS/S2_CLOUD_PROBABILITY')
 
-LNU_lighting_complex = {
-    'longitude': -122.237303,
-    'latitude': 38.593546,
-    'start': '2020-08-17T06:40',
-    'end': '2020-09-02T19:54',
-}
-RECTANGULAR_SIZE = 0.3
-
-MAX_CLOUD_PROBABILITY = 65
-region = ee.Geometry.Rectangle(coords=[LNU_lighting_complex.get('longitude') - RECTANGULAR_SIZE,
-                                       LNU_lighting_complex.get('latitude') - RECTANGULAR_SIZE,
-                                       LNU_lighting_complex.get('longitude') + RECTANGULAR_SIZE,
-                                       LNU_lighting_complex.get('latitude') + RECTANGULAR_SIZE],
-                               geodesic=False,
-                               proj=None)
-
 
 def maskClouds(img):
     clouds = ee.Image(img.get('cloud_mask')).select('probability');
-    isNotCloud = clouds.lt(MAX_CLOUD_PROBABILITY);
+    isNotCloud = clouds.lt(max_cloud_probability);
     return img.updateMask(isNotCloud)
 
 
@@ -37,62 +33,38 @@ def maskEdges(s2_img):
         s2_img.select('B8A')
             .mask()
             .updateMask(s2_img.select('B9')
-                        .mask()
-                        )
+                        .mask())
     )
 
 
-s2Sr = sentinel2a.filterDate(LNU_lighting_complex.get('start'), LNU_lighting_complex.get('end')) \
-    .filterBounds(region) \
-    .map(maskEdges)
-s2_clouds = s2_clouds.filterDate(LNU_lighting_complex.get('start'), LNU_lighting_complex.get('end')) \
-    .filterBounds(region)
+s2a_sr = sentinel2a.filterDate(start_time, end_time).filterBounds(point).map(maskEdges)
+s2c_sr = sentinel2c.filterDate(start_time, end_time).filterBounds(point).map(maskEdges)
+s2_clouds = s2_clouds.filterDate(start_time, end_time).filterBounds(point)
 
-s2SrWithCloudMask = ee.Join.saveFirst('cloud_mask')\
-    .apply(s2Sr, s2_clouds, ee.Filter.equals(leftField='system:index', rightField='system:index'))
+s2a_sr_with_cloud_mask = ee.Join.saveFirst('cloud_mask') \
+    .apply(s2a_sr, s2_clouds, ee.Filter.equals(leftField='system:index', rightField='system:index'))
 
-s2CloudMasked = ee.ImageCollection(
-    s2SrWithCloudMask).map(maskClouds).median()
+s2c_sr_with_cloud_mask = ee.Join.saveFirst('cloud_mask') \
+    .apply(s2c_sr, s2_clouds, ee.Filter.equals(leftField='system:index', rightField='system:index'))
+
+s2a_cloud_masked_collection = ee.ImageCollection(s2a_sr_with_cloud_mask).map(maskClouds)
+s2c_cloud_masked_collection = ee.ImageCollection(s2c_sr_with_cloud_mask).map(maskClouds)
+
+bands = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B9', 'B10', 'B11', 'B12', 'QA10', 'QA20', 'QA60']
+label = 'landcover'
+feature_names = list(bands)
+feature_names.append(label)
+
+
+s2a_cloud_masked = ee.ImageCollection(s2a_sr_with_cloud_mask).map(maskClouds).median()
+s2c_cloud_masked = ee.ImageCollection(s2c_sr_with_cloud_mask).map(maskClouds).median()
+
 rgbVis = {'min': 0, 'max': 3000, 'bands': ['B4', 'B3', 'B2']}
 
-#Visulization
-map_client = eeMapClient({'longitude': -122.237303,
-                          'latitude': 38.593546})
-map_client.add_to_map(s2CloudMasked, rgbVis, "s2CloudMasked")
+map_client = eeMapClient({'longitude': longitude, 'latitude': latitude})
+# map_client.download_image_collection_to_gcloud(s2a_cloud_masked_collection, rectangular_size, "Cal_fire_", feature_names)
+# map_client.download_image_to_gcloud(s2c_cloud_masked, rectangular_size, "Cal_fire_")
 
-# USER_NAME = 'test'
-# OUTPUT_BUCKET = 'zhaoyutimtest'
-# BANDS = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7']
-# LABEL = 'landcover'
-# N_CLASSES = 3
-# FEATURE_NAMES = list(BANDS)
-# FEATURE_NAMES.append(LABEL)
-# file_extension = '.tfrecord.gz'
-# IMAGE_FILE_PREFIX = 'Cal_fire_'
-# EXPORT_REGION = ee.Geometry.Rectangle([LNU_lighting_complex.get('longitude') - RECTANGULAR_SIZE,
-#                                        LNU_lighting_complex.get('latitude') - RECTANGULAR_SIZE,
-#                                        LNU_lighting_complex.get('longitude') + RECTANGULAR_SIZE,
-#                                        LNU_lighting_complex.get('latitude') + RECTANGULAR_SIZE])
-#
-# pprint({'training': s2CloudMasked.getInfo()})
-# print('Found Cloud Storage bucket.' if tf.io.gfile.exists('gs://' + OUTPUT_BUCKET)
-#     else 'Can not find output Cloud Storage bucket.')
-#
-# # Setup the task.
-# image_task = ee.batch.Export.image.toCloudStorage(
-#   image=s2CloudMasked,
-#   description='Image Export',
-#   fileNamePrefix=IMAGE_FILE_PREFIX,
-#   bucket=OUTPUT_BUCKET,
-#   scale=30,
-#   fileFormat='GeoTIFF',
-#   region=EXPORT_REGION.toGeoJSON()['coordinates'],
-# )
-#
-# image_task.start()
-#
-# import time
-# while image_task.active():
-#   print('Polling for task (id: {}).'.format(image_task.id))
-#   time.sleep(30)
-# print('Done with image export.')
+# # Visualization
+map_client.add_to_map(s2a_cloud_masked, rgbVis, "s2CloudMasked")
+
--- eeMapClient.py
+++ eeMapClient.py
@@ -1,6 +1,15 @@
+from pprint import pprint
+
 import folium
 import ee
 import webbrowser
+import tensorflow as tf
+import time
+import yaml
+
+# Load configuration file
+with open("configuration.yml", "r", encoding="utf8") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
 
 
 class eeMapClient:
@@ -12,6 +21,12 @@ class eeMapClient:
         self.map = folium.Map(location=[self.latitude, self.longitude], zoom_start=12)
 
     def add_ee_layer(self, ee_image_object, vis_params, name):
+        '''
+        :param ee_image_object: Image in GEE
+        :param vis_params: Visualisation parameters
+        :param name: The name of the Layer, as it will appear in LayerControls
+        :return:
+        '''
         map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
         folium.raster_layers.TileLayer(
             tiles=map_id_dict['tile_fetcher'].url_format,
@@ -27,3 +42,72 @@ class eeMapClient:
         outHtml = '/Users/zhaoyu/PycharmProjects/CalFireMonitoring/map.html'
         self.map.save(outHtml)
         webbrowser.open('file://' + outHtml)
+
+    def download_image_to_gcloud(self, img, size, filename_prefix):
+        '''
+        Export images to google cloud, the output image is a rectangular with the center at given latitude and longitude
+        :param img: Image in GEE
+        :param size: The longth beteen the edge of the rectangular and the center
+        :param filename_prefix: The filename prefix in Google cloud
+        :return: None
+        '''
+        export_region = ee.Geometry.Rectangle([self.longitude - size,
+                                               self.latitude - size,
+                                               self.longitude + size,
+                                               self.latitude + size])
+
+        pprint({'Image info:': img.getInfo()})
+        print('Found Cloud Storage bucket.' if tf.io.gfile.exists('gs://' + config.get('output_bucket'))
+              else 'Can not find output Cloud Storage bucket.')
+
+        # Setup the task.
+        image_task = ee.batch.Export.image.toCloudStorage(
+            image=img,
+            description='Image Export',
+            fileNamePrefix=filename_prefix,
+            bucket=config.get('output_bucket'),
+            scale=30,
+            fileFormat='GeoTIFF',
+            region=export_region.toGeoJSON()['coordinates'],
+        )
+
+        image_task.start()
+
+        while image_task.active():
+            print('Polling for task (id: {}).'.format(image_task.id))
+            time.sleep(30)
+        print('Done with image export.')
+
+    def download_image_collection_to_gcloud(self, img_collection, size, filename_prefix, feature_names):
+        '''
+        Export images to google cloud, the output image is a rectangular with the center at given latitude and longitude
+        :param img: Image in GEE
+        :param size: The longth beteen the edge of the rectangular and the center
+        :param filename_prefix: The filename prefix in Google cloud
+        :return: None
+        '''
+        export_region = ee.Geometry.Rectangle([self.longitude - size,
+                                               self.latitude - size,
+                                               self.longitude + size,
+                                               self.latitude + size])
+
+        pprint({'Image info:': img_collection.getInfo()})
+        print('Found Cloud Storage bucket.' if tf.io.gfile.exists('gs://' + config.get('output_bucket'))
+              else 'Can not find output Cloud Storage bucket.')
+
+        # Setup the task.
+        collection_task = ee.batch.Export.table.toCloudStorage(
+            collection=img_collection,
+            description='Training Export',
+            fileNamePrefix=filename_prefix,
+            bucket=config.get('output_bucket'),
+            fileFormat='TFRecord',
+            selectors=feature_names
+        )
+
+        collection_task.start()
+
+        while collection_task.active():
+            print('Polling for task (id: {}).'.format(collection_task.id))
+            time.sleep(30)
+        print('Done with image export.')
